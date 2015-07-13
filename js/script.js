2dc035a11b4875534d79c01b80e5f9daf2ebbbb8
andrey-zaytcev <andrey-zaytcev@narod.ru>
2015-07-13 18:45:27 +0300

    selected items save to ids array

--- js/script.js
+++ js/script.js
@@ -2,25 +2,45 @@ $(document).ready( function() {
 
     console.log( 'On ready' );
     
-    function RangeSelector(){
+	function CreateMultipleSelectors()
+	{
+	}
+	
+	function RangeSelector($wrapperfilter){
         var elements = [];
         console.log( 'call RangeSelector' );
-        var rangeValues_li = $("#filter-bar li");
-        rangeValues_li.removeClass("active");
-        console.log( rangeValues_li );
-        rangeValues_li.each( function( index ) {
-        console.log( index + ": " + $( this ).text() );        
-        elements.push( this ); });        
+		console.log( $wrapperfilter );
+        var $li = $wrapperfilter.children("ul#filter-bar").children("li.filter-option");
+		console.log('li: ' + $li.text());
+        $li.each(function(index){
+			console.log( index + ": " + $( this ).text() );
+			elements.push( this );});
+		var selector = this;
+		this.toggle = function($element) {
+			console.log('in toggle');
+		}
         this.min_element = elements[0];
         this.max_element = elements[elements.length - 1];        
     }
     
-    var obj = new RangeSelector();
-    $(obj.min_element).addClass("active");
-    $(obj.max_element).addClass("active");
+    var obj = new RangeSelector($("div#wrapper-filter"));
     
     $("#filter-bar li").click(function(){
-        //$("#filter-bar li").removeClass("active");
-        $(this).addClass("active");
+        $(this).hasClass("active")? 
+		$(this).removeClass("active"): $(this).addClass("active");
+		$(this).parent().children().each( function( index ) {
+        console.log( index + ": " + $( this ).text() );
+		});
+		$(this).each( function( index ) {
+        console.log( index + "this : " + $( this ).text() );
+		});
+		var $ids = [];
+		var $parent = $(this).parent();
+		$parent.children().each( function( index ) {
+			if($(this).hasClass("active"))
+				$ids.push($(this).attr("data-target"));
+		});		
+		$parent.attr("data-ids", $ids);
+		console.log( "ids: " + $ids );
     });
 })
