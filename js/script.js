3c791af54da256e4f0ddd904f98222c73cc8cd15
andrey-zaytcev <andrey-zaytcev@narod.ru>
2015-07-14 20:41:20 +0300

    implemented toggle selector

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 $(document).ready( function() {
-
-    console.log( 'On ready' );
     
 	function CreateMultipleSelectors()
 	{
+		var selectors = [];
+		$("div#wrapper-filter").each(function(index) {
+			var select_method = $(this).attr("select-method");
+			switch (select_method) {
+				case "multiple":
+					selectors.push(new MultipleSelectControl($(this)));
+					break;
+				case "toggle":
+					selectors.push(new ToggleSelectControl($(this)));
+					break;
+				default:
+					throw new Error("Selector " + select_method + " not implemented");
+			}
+		});
 	}
     
-    function ISelectControl($wrapperfilter) {                      
+    function ISelectControl( $wrapperfilter ) {                      
         this._$ul = $($wrapperfilter).children("ul#filter-bar");
         this._$li = this._$ul.children("li.filter-option");
     }
     ISelectControl.prototype.Select = function($element) {
-        alert(this);
+         throw new Error("ISelectControl.Select not implemented");
     } 
     
     function MultipleSelectControl($wrapperfilter){
@@ -30,23 +42,43 @@ $(document).ready( function() {
     
     MultipleSelectControl.prototype.Select = function($element) {
         $element.hasClass("active")? 
-        $element.removeClass("active"): $element.addClass("active");
-        $element.parent().children().each( function( index ) {
+		$element.removeClass("active"): $element.addClass("active");
+        this._$li.each( function( index ) {
             console.log( index + ": " + $( this ).text() );
         });
         $element.each( function( index ) {
             console.log( index + "this : " + $( this ).text() );
         });
         var $ids = [];            
-        this._$ul.children().each( function( index ) {
-        if($element.hasClass("active"))
-            $ids.push($element.attr("data-target"));
-        });		
+        this._$li.each( function( index ) {
+			if ($(this).hasClass("active"))
+				$ids.push($(this).attr("data-target"));
+        });
+        this._$ul.attr("data-ids", $ids);
+        console.log( "ids: " + $ids );
+    }
+	
+	function ToggleSelectControl($wrapperfilter){
+        ISelectControl.apply(this, $wrapperfilter);
+		var control_ = this;
+        this._$li.click(function(){
+            control_.Select($(this));
+        });
+    }
+	
+	ToggleSelectControl.prototype = Object.create(ISelectControl.prototype);
+    ToggleSelectControl.prototype.constructor = ToggleSelectControl;
+    
+    ToggleSelectControl.prototype.Select = function($element) {
+		this._$li.removeClass("active");
+		$element.addClass("active");
+        var $ids = [];            
+		$ids.push($element.attr("data-target"));
         this._$ul.attr("data-ids", $ids);
         console.log( "ids: " + $ids );
     }
     
-    var obj = new MultipleSelectControl($("div#wrapper-filter"));
+	CreateMultipleSelectors();
     
     /*$("#filter-bar li").click(function(){
         $(this).hasClass("active")? 
