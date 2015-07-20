c0d3179427b8309d2e1722ae170a814aa7e938ab
andrey-zaytcev <andrey-zaytcev@narod.ru>
2015-07-20 22:48:31 +0300

    impplemented storing indexes in data-target attribute

--- js/script.js
+++ js/script.js
@@ -22,34 +22,25 @@ $(document).ready( function() {
         this._$ul = $($wrapperfilter).children("ul#filter-bar");
         this._$li = this._$ul.children("li.filter-option");
         this._data_target = [];
-        var dt_str = this._$ul.attr( "data-target" ) ? this._$ul.attr( "data-target" ) : "";
-        this._data_target = dt_str.split(",");
+        var dt_str = this._$ul.attr( "data-target" ) ? this._$ul.attr( "data-target" ) : "";        
+        this._data_target = dt_str.split(',');
+        
         this._get_li_by_target = function( data_target ) {
-        	this._$li.each( function( index ) {
-        		console.log( "_get_li_by_target with " + data_target );
-        		console.log( "li attr is " + $( this ).attr( "data-target" ) );
-                if ( $( this ).attr( "data-target" ) === data_target ) {
-                	return $(this);
-        		}
-            });
+        	return this._$ul.children("li.filter-option[data-target='" + data_target + "']");
         }
     }
+    
     ISelectControl.prototype.Select = function($element) {
          throw new Error("ISelectControl.Select not implemented");
     } 
     
     function MultipleSelectControl($wrapperfilter){
         ISelectControl.apply(this, $wrapperfilter);
-        console.log( 'call MultipleSelectControl' );
-        console.log('$ul : ' + this._$ul.text());
-		console.log('li: ' + this._$li.text());
 		
 		for ( var id in this._data_target ) {
-			console.log( "active target is " + this._data_target[ id ] );
-			var element = this._get_li_by_target( this._data_target[ id ] );
-			console.log( element );
-			//this.Select( this._get_li_by_target( target ) );
-		}			
+			var $element = this._get_li_by_target( this._data_target[ id ] );
+			$element.addClass("active");
+		}
 		
 		var control_ = this;
         this._$li.click(function(){
@@ -61,26 +52,26 @@ $(document).ready( function() {
     MultipleSelectControl.prototype.constructor = MultipleSelectControl;
     
     MultipleSelectControl.prototype.Select = function($element) {
-        $element.hasClass("active")? 
-		$element.removeClass("active"): $element.addClass("active");
-        this._$li.each( function( index ) {
-            console.log( index + ": " + $( this ).text() );
-        });
-        $element.each( function( index ) {
-            console.log( index + "this : " + $( this ).text() );
-        });            
-        var data_target = this._data_target;
-        this._$li.each( function( index ) {
-			if ($(this).hasClass("active"))
-				console.log( "data_target is " + data_target );
-				data_target.push( $(this).attr("data-target") );
-        });
+    	if ( $element.hasClass("active") ) {
+			$element.removeClass("active");
+			var index = this._data_target.indexOf( $element.attr("data-target") );
+			this._data_target.splice( index, 1 );
+		} else {
+			$element.addClass("active");
+			this._data_target.push( $element.attr("data-target") );
+		}
+        
         this._$ul.attr( "data-target", this._data_target );
-        console.log( "data-target: " + this._data_target );
     }
 	
-	function ToggleSelectControl($wrapperfilter){
+    function ToggleSelectControl($wrapperfilter){
         ISelectControl.apply(this, $wrapperfilter);
+		
+		if ( this._data_target.length > 1 )
+			throw new Error("ToggleSelectControl can not have more than one element in data-target attribute");
+		
+		this._get_li_by_target( this._data_target[ 0 ] ).addClass("active");
+
 		var control_ = this;
         this._$li.click(function(){
             control_.Select($(this));
@@ -91,32 +82,12 @@ $(document).ready( function() {
     ToggleSelectControl.prototype.constructor = ToggleSelectControl;
     
     ToggleSelectControl.prototype.Select = function($element) {
-		this._$li.removeClass("active");
-		$element.addClass("active");            
-		this._data_target.push($element.attr("data-target"));
+    	this._$li.removeClass("active");
+    	$element.addClass("active");     
+    	this._data_target = $element.attr("data-target");
         this._$ul.attr( "data-target", this._data_target );
-        console.log( "data-target: " + this._data_target );
+        console.log( "ToggleSelectControl::data-target: " + this._data_target );
     }
     
 	CreateMultipleSelectors();
-    
-    /*$("#filter-bar li").click(function(){
-        $(this).hasClass("active")? 
-		$(this).removeClass("active"): $(this).addClass("active");
-		$(this).parent().children().each( function( index ) {
-        console.log( index + ": " + $( this ).text() );
-		});
-		$(this).each( function( index ) {
-        console.log( index + "this : " + $( this ).text() );
-		});
-		var ids = [];
-		var $parent = $(this).parent();
-		$parent.children().each( function( index ) {
-			if($(this).hasClass("active"))
-				ids.push($(this).attr("data-target"));
-		});		
-		$parent.attr("data-ids", ids);
-		console.log( "ids: " + ids );
-        obj.Select($(this));
-    });*/
 })
